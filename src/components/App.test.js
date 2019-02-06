60fc77c1507307a4647982007a993f728449bca8
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2019-02-06 18:37:56 +0100

    Add react-testing-library

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { render } from 'react-testing-library'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  render(<App />, div)
+it('renders <App /> without crashing', () => {
+  render(<App />)
 })
