7d6e834d13e9d5a54fdc5f2e86c2da1a5594c24a
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2019-03-01 19:10:08 +0100

    Refactored App component to function declaration

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,29 @@ import { hot } from 'react-hot-loader/root'
 
 import './App.scss'
 
-const App = () => (
-  <div className="App">
-    <header className="main-header">
-      <h1 className="banner">
-        <span className="banner__title">
-          react-bowl{' '}
-          <span role="img" aria-label="Steaming Bowl emoji">
-            🍜
+function App() {
+  return (
+    <div className="App">
+      <header className="main-header">
+        <h1 className="banner">
+          <span className="banner__title">
+            react-bowl{' '}
+            <span role="img" aria-label="Steaming Bowl emoji">
+              🍜
+            </span>
           </span>
-        </span>
-        <span className="banner__caption">An React Boilerplate</span>
-      </h1>
-    </header>
+          <span className="banner__caption">An React Boilerplate</span>
+        </h1>
+      </header>
 
-    <div className="main-content">
-      <p>Welcome to the `react-bowl` - An React Boilerplate.</p>
-      <p>
-        <code>Let the Code begins</code>
-      </p>
+      <div className="main-content">
+        <p>Welcome to the `react-bowl` - An React Boilerplate.</p>
+        <p>
+          <code>Let the Code begins</code>
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default hot(App)
