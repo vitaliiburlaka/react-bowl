b461b742bb3bf8abceff8a06123b4456db0151c6
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2019-07-10 18:30:10 +0200

    Bumped dependencies versions

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render } from '@testing-library/react'
 import App from './App'
 
 it('renders <App /> without crashing', () => {
