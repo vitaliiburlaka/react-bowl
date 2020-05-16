3eda464ff2a746f8c937f2c2543844c7f531d817
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2020-05-16 20:01:21 +0200

    chore: Changes in jest.config.js. Add files and SVGR mocks

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!./setupTests.js',
-    '!src/index.js',
-  ],
+  collectCoverageFrom: ['src/**/*.+(js|jsx|ts|tsx)', '!src/index.js'],
   testEnvironment: 'jsdom',
   testURL: 'http://localhost',
   transform: {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleNameMapper: {
-    '.+\\.(css|svg|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'identity-obj-proxy',
+    '\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.svg': '<rootDir>/src/__mocks__/svgrMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
   },
   moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-  verbose: false,
 }
